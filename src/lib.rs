535da15b612194c9d3830ccc34a9921f1103a81a
git <trantuantubk@gmail.com>
2025-12-26 12:07:39 -0500

    replace the closure with the associate function path in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,5 @@ pub async fn run(listener: TcpListener) -> Result<(), std::io::Error> {
     let app = Router::new().route("/health", get(handlers::health::health_check));
     axum::serve(listener, app)
         .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
+        .map_err(std::io::Error::other)
 }
